783ea3ad93f997398d5ce0a0068291ad177ef3b9
ReivesD <129226363+ReivesD@users.noreply.github.com>
2023-06-28 22:24:34 -0400

    Add files via upload

---
+++ Module 10 Challenge/app.py
@@ -0,0 +1,36 @@
+# Import the dependencies.
+from flask import Flask, render_template
+import pandas as pd
+import matplotlib.pyplot as plt
+
+app = Flask(__name__)
+
+
+
+
+#################################################
+# Database Setup
+#################################################
+
+
+# reflect an existing database into a new model
+
+# reflect the tables
+
+
+# Save references to each table
+
+
+# Create our session (link) from Python to the DB
+
+
+#################################################
+# Flask Setup
+#################################################
+
+
+
+
+#################################################
+# Flask Routes
+#################################################
